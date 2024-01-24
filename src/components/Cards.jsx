ca4ede4e3ead0ee71fa4882199908bfb1192ceab
Hélio Júnior <heliorpjunior116@gmail.com>
2024-01-24 12:54:39 -0300

    feat: implementação de um card

---
+++ src/components/Cards.jsx
@@ -0,0 +1,15 @@
+import "./Cards.css";
+
+function Cards() {
+  return (
+    <div className="card">
+      <h2>Rick Sanchez</h2>
+      <img
+        className="image"
+        src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+      />
+    </div>
+  );
+}
+
+export default Cards;
